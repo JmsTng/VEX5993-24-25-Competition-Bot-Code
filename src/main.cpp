bb6b00d0deba371612bdcba87da599d065502ada
JmsTng <95390086+JmsTng@users.noreply.github.com>
2024-11-21 12:39:39 -0500

    Reorganized to adhere to competition format
    Need more work on pneumatic systems

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,15 @@
 /*                  HIGH STAKES                                               */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
-#include <cmath>
+
 #include "vex.h"
 
 using namespace vex;
 
-// ******************************* DEFINITIONS ******************************* //
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
 brain Brain; // A global instance of vex::brain used for printing to the V5 brain screen
 
 // Motors
@@ -25,7 +28,7 @@ motor Right2 = motor(PORT20, ratio18_1, true);
 motor Intake1 = motor(PORT11, ratio18_1, false); // upper
 motor Intake2 = motor(PORT12, ratio18_1, true); // lower
 
-motor Claw = motor(PORT15, ratio18_1, false);
+// motor Claw = motor(PORT15, ratio18_1, false);
 
 // Group motors together into groups for easier control
 motor_group Left = motor_group(Left1, Left2);
@@ -33,38 +36,17 @@ motor_group Right = motor_group(Right1, Right2);
 
 motor_group Intake = motor_group(Intake1, Intake2);
 
+// Pneumatic control
+digital_out Claw = digital_out(Brain.ThreeWirePort.A);
 
 
 // Other devices
 bool remoteEnable = true;
-competition Competition = competition();
 controller Controller = controller(primary);
 int deadzone = 5;
 
-// Tests
-void testSpinAll() {
-    Left1.spin(forward, 100, percent);
-    Left2.spin(forward, 100, percent);
-    Right1.spin(forward, 100, percent);
-    Right2.spin(forward, 100, percent);
 
-    Brain.Screen.print("Spin drive");
-    Brain.Screen.newLine();
-
-    Claw.spinTo(90, degrees);
-    Claw.spinTo(0, degrees);
-}
-
-
-// Common Instructions
-/**
- * Stops all drivetrain motors.
- */
-void driveStop() {
-    Left.stop();
-    Right.stop();
-}
-
+/* HELPER FUNCTIONS */
 /**
  * Spins left-side motors.
  *
@@ -101,23 +83,90 @@ void toggleIntake() {
 void toggleClaw() {
     static short grabbing = 1; // 1 when not grabbing, -1 when grabbing
 
-    Claw.spinTo(120 * grabbing, degrees);
-    grabbing *= -1;
+    Claw.set(grabbing);
+    grabbing ^= 1;
 }
 
 
-// Main
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (1) {
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    int x = abs(Controller.Axis1.position()) < deadzone ? 0 : Controller.Axis1.position();
+    int y = abs(Controller.Axis2.position()) < deadzone ? 0 : Controller.Axis2.position();
+
+    x = x * abs(x) / 100;
+    y = y * abs(y) / 100;
+
+    driveLeft(y - x);
+    driveRight(y + x);
+
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
 int main() {
-    Controller.ButtonL1.pressed(toggleIntake);
-    Controller.ButtonR1.pressed(toggleClaw);
-    
-    while (1) {
-        int x = abs(Controller.Axis1.position()) < deadzone ? 0 : Controller.Axis1.position();
-        int y = abs(Controller.Axis2.position()) < deadzone ? 0 : Controller.Axis2.position();
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
 
-        driveLeft(y - x);
-        driveRight(y + x);
+  Controller.ButtonL1.pressed(toggleIntake);
+  Controller.ButtonR1.pressed(toggleClaw);
 
-        wait(5, msec);
-    }
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
 }
